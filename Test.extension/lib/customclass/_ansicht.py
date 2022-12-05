cdf788231bcbfe94b9c110e4e7349ad6350a950b
MenghuiZhang <zhangmenghui11@gmail.com>
2022-12-05 18:08:19 +0100

    export Bauteilliste

--- Test.extension/lib/customclass/_ansicht.py
+++ Test.extension/lib/customclass/_ansicht.py
@@ -9,6 +9,15 @@ class RVItem(RMitem):
         self.elem = doc.GetElement(self.elemid)
         self.typ = self.elem.ViewType
 
+class Data:
+    def __init__(self,data,background = [255,255,255],textcolor = [0,0,0],units = '',textalign = 'Center',accuracy = 1.0):
+        self.data = data
+        self.background = background
+        self.textcolor = textcolor
+        self.units = units
+        self.textalign = textalign
+        self.accuracy = accuracy
+
 class RBLItem(RVItem):
     def __init__(self,name,elemid,doc):
         RVItem.__init__(self,name,elemid,doc)
@@ -16,10 +25,9 @@ class RBLItem(RVItem):
     def get_Params(self):
         return self.elem.Definition.GetFieldOrder()
     
-    def get_ParamsType(self):
+    def get_column_format(self):
         params = self.get_Params()
-        dict_params = {self.elem.Definition.GetField(param).ColumnHeading:\
-            self.elem.Definition.GetField(param).CanDisplayMinMax() for param in params}
+        dict_params = {self.elem.Definition.GetField(param).ColumnHeading:self.elem.Definition.GetField(param) for param in params}
         return dict_params
     
     def get_Data(self):
@@ -34,23 +42,68 @@ class RBLItem(RVItem):
 
     def get_Data2(self):
         Liste = []
-        dict_params = self.get_ParamsType()
+        dict_params = self.get_column_format()
         tableData = self.elem.GetTableData()
         sectionBody = tableData.GetSectionData(DB.SectionType.Body)
         headers = [self.elem.GetCellText(DB.SectionType.Body, 0, c) \
                 for c in range(sectionBody.NumberOfColumns)]
-        Liste.append(headers)
+        Headerlist = []
+        for c in range(sectionBody.NumberOfColumns):
+            celltext = self.elem.GetCellText(DB.SectionType.Body, 0, c)
+            headerformat = sectionBody.GetTableCellStyle(0,c)
+            Background = [headerformat.BackgroundColor.Red,headerformat.BackgroundColor.Green,headerformat.BackgroundColor.Blue]
+            Textcolor = [headerformat.TextColor.Red,headerformat.TextColor.Green,headerformat.TextColor.Blue]
+            Headerlist.append(Data(celltext,Background,Textcolor))
+        Liste.append(Headerlist)
         for r in range(1,sectionBody.NumberOfRows):
             rowlist = []
             for c in range(sectionBody.NumberOfColumns):
                 celltext = self.elem.GetCellText(DB.SectionType.Body, r, c)
-                if dict_params[headers[c]] == True:
-                    if celltext.find('%') == -1:
-                        try:celltext = float(celltext[:celltext.find(' ')])
+                cellformat = sectionBody.GetTableCellStyle(r,c)
+                Background = [cellformat.BackgroundColor.Red,cellformat.BackgroundColor.Green,cellformat.BackgroundColor.Blue]
+                Textcolor = [cellformat.TextColor.Red,cellformat.TextColor.Green,cellformat.TextColor.Blue]
+                
+                textalignment = dict_params[headers[c]].HorizontalAlignment.ToString()
+                Headerformat = dict_params[headers[c]].GetFormatOptions()
+                try:
+                    Accuracy = Headerformat.Accuracy
+                except:
+                    Accuracy = ''
+                try:
+                    unit = DB.LabelUtils.GetLabelFor(Headerformat.UnitSymbol)
+                except:
+                    unit = ''
+                if dict_params[headers[c]].CanDisplayMinMax() == True:
+                    
+                    if unit:
+                        if unit == '%':
+                            try:
+                                celltext = float(celltext.replace(unit,''))/100
+                            except:
+                                try:
+                                    celltext = float(celltext)/100
+                                except:celltext = celltext
+                        try:
+                            celltext = float(celltext.replace(' '+unit,''))
                         except:
-                            try:celltext = float(celltext)
-                            except:pass
-                rowlist.append(celltext)
+                            try:
+                                celltext = float(celltext)
+                            except:celltext = celltext
+                    
+
+                        # if celltext.find('%') == -1:
+                        #     try:celltext = float(celltext[:celltext.find(' ')])
+                        #     except:
+                        #         try:celltext = float(celltext)
+                        #         except:pass
+                    else:
+                        try:
+                            celltext = float(celltext)
+                        except:celltext = celltext
+                cellformat.Dispose()
+                
+
+                rowlist.append(Data(celltext,Background,Textcolor,unit,textalignment,Accuracy))
             Liste.append(rowlist)
         return Liste
                 
